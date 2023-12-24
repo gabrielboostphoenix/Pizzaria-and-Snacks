fa8c155bcc7ad3f23c5433de464525b7ebf37460
gabrielboostphoenix <102393858+gabrielboostphoenix@users.noreply.github.com>
2023-12-23 22:31:44 -0300

    Update users.service.ts

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -3,15 +3,33 @@ import { signUp } from './dto/signup';
 import { prisma } from '../../prisma/prismaClient';
 
 const addUser = async (data: signUp) => {
-    await prisma
+    await prisma.users.create({
+        data: {
+            name: data.userName,
+            email: data.userEmail,
+            password: data.userPassword
+        }
+    });
 }
 
 const deleteUser = async (data: signIn) => {
-
+    await prisma.users.findMany({
+        data: {
+            email: data.userEmail,
+            password: data.userPassword
+        }
+    });
 }
 
-const changeName = async (data: signIn) => {
-
+const changeName = async (data: signUp) => {
+    await prisma.users.update({
+        where: {
+            email: data.userEmail
+        },
+        data: {
+            name: data.userName
+        }
+    })
 }
 
 export { addUser, deleteUser, changeName };
